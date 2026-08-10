f4c53142668bb12b801ad803e0122ba67892c27b
MKJ <hotmass81@gmail.com>
2026-08-10 03:19:33 +0300

    error

--- matoleo_system/matoleo_system/settings.py
+++ matoleo_system/matoleo_system/settings.py
@@ -6,7 +6,7 @@ from pathlib import Path
 import os
 import dj_database_url
 
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get(
     'SECRET_KEY',
