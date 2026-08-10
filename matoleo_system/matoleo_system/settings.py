f32269765cf740ca368dd757fdad6351ad655341
MKJ <hotmass81@gmail.com>
2026-08-10 08:39:52 +0300

    fix error

--- matoleo_system/matoleo_system/settings.py
+++ matoleo_system/matoleo_system/settings.py
@@ -128,6 +128,7 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+WHITENOISE_USE_FINDERS = True
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
